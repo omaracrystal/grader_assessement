e8b80b889ee039836b231e25c558965bb1775c38
Crystal <crystal@Zachs-MacBook-Air.local>
2015-11-03 02:46:43 -0700

    added jasmine tests

---
+++ grader.js
@@ -0,0 +1,96 @@
+(function(){
+
+  'use strict';
+
+  function letterGrader (score) {
+    if (score > 96) {
+      return "A+";
+    } else if (score > 92) {
+      return "A";
+    } else if (score > 89) {
+      return "A-";
+    } else if (score > 86) {
+      return "B+";
+    } else if (score > 82) {
+      return "B";
+    } else if (score > 79) {
+      return "B-";
+    } else if (score > 76) {
+      return "C+";
+    } else if (score > 72) {
+      return "C";
+    } else if (score > 69) {
+      return "C-";
+    } else if (score > 66) {
+      return "D+";
+    } else if (score > 62) {
+      return "D";
+    } else if (score > 59) {
+      return "D-";
+    } else {
+      return "F";
+    }
+  }
+
+
+  function averageScore (scoreArr){
+    var total = 0;
+    for (var i = 0; i < scoreArr.length; i++) {
+      total += scoreArr[i];
+    }
+    return total/scoreArr.length;
+  }
+
+
+  function medianScore (scoreArr) {
+    //sort array of numbers from lowest to highest
+    var sortArr = scoreArr.sort(function(a,b) {
+      return a-b;
+    });
+    //check to see if array length is even or odd
+    if (sortArr.length  % 2 === 0) {
+      //divide array into two even arrays
+      var left = sortArr.splice(0,sortArr.length/2);
+      var right = sortArr;
+      var middleAvg = (left[left.length-1]+right[0])/2;
+      //return middle average
+      return middleAvg;
+    } else {
+      //return middle
+      return sortArr[Math.floor((sortArr.length -1)/ 2)];
+    }
+  }
+
+
+  function modeScore (scoreArr) {
+    var count = {};
+    var max = 0;
+    var result = [];
+    //iterate through array and count up the occurances of each element
+    scoreArr.forEach(function (i) {
+        count[i] = (count[i] || 0) + 1;
+        //if element has most occurances assign value of max
+        if (count[i] > max) {
+            max = count[i];
+        }
+    });
+    //iterate through object count and pushing max into result array if there are more than two max occurances both will be pushed
+    Object.keys(count).forEach(function (k) {
+        count[k] === max && result.push(k);
+    });
+    //converting results from an array of strings to integers
+    var mode = result.map(Number);
+    return mode;
+  }
+
+
+  module.exports = {
+    letterGrader: letterGrader,
+    averageScore: averageScore,
+    medianScore: medianScore,
+    modeScore: modeScore
+  };
+
+})();
+
+
---
+++ grader.spec.js
@@ -0,0 +1,45 @@
+var grader = require('./grader');
+
+// tests!
+describe('Letter Grader', function(){
+  it("returns a letter grade equivalent with a numerical test score", function(){
+    expect(grader.letterGrader(105)).toEqual("A+");
+    expect(grader.letterGrader(99)).toEqual("A+");
+    expect(grader.letterGrader(95)).toEqual("A");
+    expect(grader.letterGrader(90)).toEqual("A-");
+    expect(grader.letterGrader(89)).toEqual("B+");
+    expect(grader.letterGrader(85)).toEqual("B");
+    expect(grader.letterGrader(80)).toEqual("B-");
+    expect(grader.letterGrader(79)).toEqual("C+");
+    expect(grader.letterGrader(75)).toEqual("C");
+    expect(grader.letterGrader(70)).toEqual("C-");
+    expect(grader.letterGrader(69)).toEqual("D+");
+    expect(grader.letterGrader(65)).toEqual("D");
+    expect(grader.letterGrader(60)).toEqual("D-");
+    expect(grader.letterGrader(59)).toEqual("F");
+  });
+});
+
+describe('Average Score', function(){
+  it("returns the average from an array of scores", function(){
+    expect(grader.averageScore([90, 95, 87, 60])).toEqual(83);
+    expect(grader.averageScore([74, 85, 79, 86])).toEqual(81);
+  });
+});
+
+
+describe('Median Score', function(){
+  it("returns the median from an array of scores", function(){
+    expect(grader.medianScore([52, 80, 80, 86, 94])).toEqual(80);
+    expect(grader.medianScore([74, 85, 79, 86])).toEqual(82);
+  });
+});
+
+
+describe('Mode Score', function(){
+  it("returns the mode from an array of scores", function(){
+    expect(grader.modeScore([82, 92, 75, 91, 92, 92, 89, 95, 95, 100, 86])).toEqual([92]);
+    expect(grader.modeScore([54, 75, 98, 75, 86, 75, 93])).toEqual([75]);
+
+  });
+});
